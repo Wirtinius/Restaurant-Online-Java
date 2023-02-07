51dff9fa78db6dcce2084d5c6e4843bb96d9fa83
Wirtinius <your_email@example.com>
2023-02-07 11:26:07 +0600

    new class in java - PostgresDB which implements the IDB interface in this class we connect the database to our project and also create tables in that DB. the class getConnection is a method from IDB interface

--- src/data/PostgresDB.java
+++ src/data/PostgresDB.java
@@ -1,4 +1,45 @@
 package data;
+import data.interfaces.IDB;
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-public class PostgresDB {
+public class PostgresDB implements IDB {
+    Connection connection = null;
+    Statement statement = null;
+    try {
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/yourdatabase", "username", "password");
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        // create table dishes
+        String sqlDishes = "CREATE TABLE dishes " +
+                "(id INT PRIMARY KEY     NOT NULL," +
+                " name           TEXT    NOT NULL, " +
+                " price          REAL    NOT NULL)";
+        try {
+            statement.executeUpdate(sqlDishes);
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+        System.out.println("Table 'dishes' created successfully");
+
+        // Establish the connection
+        Connection con = DriverManager.getConnection(connectionUrl, "postgres", "91926499");
+
+        return con;
+    } catch (Exception e) {
+        System.out.println(e);
+        return null;
+    }
+}
 }
