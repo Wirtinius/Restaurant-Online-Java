17ff62676fed9887de22031938b11b53978d2d76
Wirtinius <your_email@example.com>
2023-02-07 11:40:18 +0600

    In IUserRepository 5 methods createClient, getClient, loginIn, getAllClients, getMenuDrinks

--- src/repositories/interfaces/IUserRepository.java
+++ src/repositories/interfaces/IUserRepository.java
@@ -1,4 +1,16 @@
 package repositories.interfaces;
 
+import entities.Client;
+import entities.Dish;
+
+import java.util.List;
+
 public interface IUserRepository {
+    boolean createClient(Client client);
+    Client getClient(int id);
+    boolean loginIn(String name, String surname);
+    List<Client> getAllClients();
+    List<Dish> getMenuDishes();
+//    List<Drink> getMenuDrinks();
+
 }
