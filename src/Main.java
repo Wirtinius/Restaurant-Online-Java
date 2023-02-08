71ec22afee66a98c4ae01ae64834d56156436dcc
Anwr23 <ganyev.2005@gmail.com>
2023-02-08 20:01:09 +0600

    main class Create and initialize objects for PostgresDB, UserRepository, UserController, and MyApplication Logging page Prompt user for name and surname, if found in clients list, welcome and direct to client log page.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
+import data.PostgresDB;
+import data.interfaces.IDB;
+import repositories.UserRepository;
+import repositories.interfaces.IUserRepository;
+import controllers.UserController;
+
+
+import data.PostgresDB;
+import data.interfaces.IDB;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+import java.sql.*;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        IDB db = new PostgresDB();
+        IUserRepository repo = new UserRepository(db);
+        UserController controller = new UserController(repo);
+        MyApplication app = new MyApplication(controller);
+        app.start();
+
+        // ========================= -Logging Page- =================================
+        Scanner input = new Scanner(System.in);
+        boolean isClient = false;
+        entities.Client loggedClient = null;
+
+
+        System.out.println("\n========================");
+        System.out.println("=   -AITU Restaurant-  =");
+        System.out.println("========================");
+
+        System.out.print("Please enter your name: ");
+        String name = input.nextLine();
+        System.out.print("Please enter your surname: ");
+        String surname = input.nextLine();
+        for (entities.Client c : clients) {
+            if (c.getName().equals(name) && c.getSurname().equals(surname)) {
+                isClient = true;
+                loggedClient = c;
+            }
+        }
+        if (isClient) {
+            System.out.printf("\nWelcome to the our restaurant %s!\n\n", loggedClient.getName());
+            clientLog(loggedClient, dishes);
+        }
+
+        //===========================================================================
     }
 }
