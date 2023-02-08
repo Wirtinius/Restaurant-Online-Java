382eb6f9dcced94a06ae43dd335e187a9080a1f7
Anwr23 <ganyev.2005@gmail.com>
2023-02-08 21:06:23 +0600

    main Create and initialize objects for PostgresDB, UserRepository, UserController, and MyApplication Logging page Prompt user for name and surname, if found in clients list, welcome and direct to client log page clientLog method has been refactored to allow a client to make orders and gifts, bring the bill and exit the restaurant. Scanner take input from the user, and a `switch` statement to handle the different options available. case1 Implement menu display and order making for client log case3 Implement bill display for client log case4 Implement exit functionality for client log

--- src/Main.java
+++ src/Main.java
@@ -52,4 +52,57 @@ public class Main {
 
         //===========================================================================
     }
+
+    public static void clientLog(entities.Client loggedClient, ArrayList<entities.Dish> dishes) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> orders = new ArrayList<Integer>();
+        boolean flag = true;
+        int summa = 0, choice1;
+        while (flag) {
+            System.out.println("1. Make an order");
+            System.out.println("2. Make a gift");
+            System.out.println("3. Bring the bill");
+            System.out.println("4. Exit");
+            System.out.print("Please choose one of the options: ");
+            int choice = input.nextInt();
+            input.nextLine();
+            switch (choice) {
+                case 1:
+                    System.out.println("\n===========================  -Menu-  ===========================");
+                    System.out.printf("|%-10s | %-30s | %-15s |%n", "No", "entities.Dish Name", "Price");
+                    for (entities.Dish dish : dishes) {
+                        System.out.printf("|%-10d | %-30s | %15f |%n", dish.getId(), dish.getName(), dish.getPrice());
+                    }
+                    while (true) {
+                        System.out.print("Make a choice (-1 for exist) :  ");
+                        choice1 = input.nextInt();
+
+                        if (choice1 == -1) {
+                            System.out.println();
+                            break;
+                        }
+
+                        orders.add(choice1 - 1);
+
+                        summa += dishes.get(choice1 - 1).getPrice();
+
+                    }
+                    break;
+                case 3:
+                    System.out.println("\n===========================  -Checkout-  ===========================");
+                    System.out.printf("|%-10s | %-30s | %-15s |%n", "No", "entities.Dish Name", "Price");
+                    for (Integer o : orders) {
+                        System.out.printf("|%-10d | %-30s | %15f |%n", dishes.get(o).getId(), dishes.get(o).getName(), dishes.get(o).getPrice());
+                    }
+                    System.out.printf("%nTotal price: %d%n%n", summa);
+                    break;
+
+                case 4:
+                    System.out.println("Goodbye");
+                    flag = false;
+            }
+        }
+            }
+
+
 }
