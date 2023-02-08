fed9605012ab4fb52f2b3efc6c3c3c7d3850907b
Anwr23 <ganyev.2005@gmail.com>
2023-02-08 22:37:39 +0600

    createClient for inserting new client into database getClient for retrieving client from database based on ID

--- src/repositories/UserRepository.java
+++ src/repositories/UserRepository.java
@@ -1,4 +1,79 @@
 package repositories;
 
-public class UserRepository {
-}
+import data.interfaces.IDB;
+import entities.Client;
+import entities.Dish;
+import repositories.interfaces.IUserRepository;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class UserRepository implements IUserRepository {
+    private final IDB db;
+
+    public UserRepository(IDB db) {
+        this.db = db;
+    }
+
+    @Override
+    public boolean createClient(Client client) {
+        Connection con = null;
+        try {
+            con = db.getConnection();
+            String sql = "INSERT INTO clients(name,surname,gender) VALUES (?,?,?)";
+            PreparedStatement st = con.prepareStatement(sql);
+
+            st.setString(1, client.getName());
+            st.setString(2, client.getSurname());
+            st.setString(3, client.getGender());
+
+            st.execute();
+            return true;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Client getClient(int id) {
+        Connection con = null;
+        try {
+            con = db.getConnection();
+            String sql = "SELECT id,name,surname,gender FROM clients WHERE id=?";
+            PreparedStatement st = con.prepareStatement(sql);
+
+            st.setInt(1, id);
+
+            ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                Client client = new Client(rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("surname"),
+                        rs.getString("gender"));
+
+                return client;
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        return null;
+    }
